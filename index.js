175aba45df0cebf84e9c1678e3e4d2180d2a6367
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2020-02-13 13:45:35 -0500

    small comment change

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ server.get('/', (req, res) => {
     res.status(200).json(songs);
 });
 //make port dynamic
-const port = process.env.PORT || 5000; //process.env.PORT for Heroku: process.env = Heroku; and PORT is Heroku's port.
+const port = process.env.PORT || 5000; //process.env.PORT uses port given from the host. In this case, process.env = Heroku (host's environment); and PORT is the port # that Heroku gives us.
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
